ceeefdf60399ed5f80d52e6247fe0aaf204983ba
Md Shagor Hossain <79301601+shagor-coder@users.noreply.github.com>
2021-12-28 22:18:28 +0600

    All Design Updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,58 @@
+// Scroll Spy
 
-$(document).ready(function(){
-    $('.button').click(function(){
-        var value =$(this).attr("data-filter");
-        if (value === 'all') {
-            $('.filter').show("1000")
-        }
-        else{
-            $(".filter").not("." + value).hide("1000");
-            $(".filter").filter("." + value).show("1000");
-        }
-    })
-    $('.nav-item').click(function(){
-        $(this).addClass('current').siblings().removeClass('current');
-        $('.navbar-collapse').removeClass('show');
-    })
-    $(window).scroll(function(){
-        $('#header').toggleClass('active', scrollY > 0)
-    })
+let section = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header .navbar-nav a')
+
+window.addEventListener('scroll', () => {
+  section.forEach((sec) => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 95
+    let height = sec.offsetHeight
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('current')
+        document
+          .querySelector('header nav a[href*= ' + id + ']')
+          .classList.add('current')
+      })
+    }
+  })
+})
+// Swiper initialization
+var swiper = new Swiper('.mySwiper', {
+  loop: true,
+  centeredSlides: false,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 2,
+    },
+  },
 })
 
+$(document).ready(function () {
+  $('.button').click(function () {
+    var value = $(this).attr('data-filter')
+    if (value === 'all') {
+      $('.filter').show('1000')
+    } else {
+      $('.filter')
+        .not('.' + value)
+        .hide('1000')
+      $('.filter')
+        .filter('.' + value)
+        .show('1000')
+    }
+  })
+})
